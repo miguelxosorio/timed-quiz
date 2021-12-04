a2d51ec1ec07e60c0d837e7599b739675665e792
vayne <miguel.osorio19@yahoo.com>
2021-12-04 13:47:26 -0600

    addended questions to fit the user story in README.md

--- README.md
+++ README.md
@@ -2,23 +2,23 @@
 
 
 ## User story
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
+AS A coding boot camp student,
+1. I WANT to take a timed quiz on JavaScript fundamentals that stores high      scores
+2. SO THAT I can gauge my progress compared to my peers
 
 
 ## Acceptance Criteria
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
+- GIVEN I am taking a code quiz
+- WHEN I click the start button
+- THEN a timer starts and I am presented with a question
+- WHEN I answer a question
+- THEN I am presented with another question
+- WHEN I answer a question incorrectly
+- THEN time is subtracted from the clock
+- WHEN all questions are answered or the timer reaches 0
+- THEN the game is over
+- WHEN the game is over
+- THEN I can save my initials and score
 
 
 
@@ -33,9 +33,12 @@ THEN I can save my initials and score
 - When question is answered another question is presented
     - if/else statements?
 - Time is subtracted to the timer when question is answered incorrectly
-    - 5 second deduction
+    - 10 second deduction
     - if/else statements?
 - Game's over
     - all questions are answered
     - time is reduced to 0
 - Player must be able to submit their Initials and Highscore
+
+## Image Source
+![alt text](http://url/to/img.png)
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-// Timer
+// Timer variables
 const startingMinutes = 1;
 let time = startingMinutes * 60;
+
+// Global variables
 let timerId = "";
 const countDownEl = document.getElementById('countdown');
 let index = 0;
@@ -13,6 +15,7 @@ var scores = [];
 var submitButtonEl = document.getElementById('submit_btn');
 var initialsBoxEl = document.getElementById('initials-box');
 showInitialEl.style.display = "none";
+var buttonEl = document.querySelector('#start_button');
 
 
 // countdown function
@@ -27,113 +30,116 @@ function updateCountdown() {
 };
 
 
-
-
-// Elements
-var buttonEl = document.querySelector('#start_button');
-
-
 // Creating the array and passing the number, questions, options, and answers
 let questions = [
     {
         numb: 1,
-        question:   "How many times does the average person laugh in a day?",
-        answer:     "13",
+        question:   "Inside which HTML element do we put the Javascript?",
+        answer:     "<script>",
         options: [
-            "Depends how happy you are",
-            "10",
-            "15",
-            "13"
+            "<scr>",
+            "<javascript>",
+            "<js>",
+            "<script>"
         ]
     },
     {
         numb: 2,
-        question:   "What is the Italian word for pie?",
-        answer:     "pizza",
+        question:   "Where is the correct place to insert a JavaScript?",
+        answer:     "Both the <head> section and the <body>",
         options: [
-            "pane",
-            "impasto",
-            "pane al forno"
+            "<head>",
+            "<body>",
+            "<section>",
+            "Both the <head> section and the <body>"
         ]
     },
     {
         numb: 3,
-        question:   "What is the coorect term for a question mark immediately followed by an exclamation mark?",
-        answer:     "Interrobang",
+        question:   "What is the correct syntax for referring to an external script called 'script.js'?",
+        answer:     "<script src ='script.js'",
         options: [
-            "Depends how happy you are",
-            "10",
-            "15"
+            "<script href ='script.js'",
+            "<script src ='script.js'",
+            "<script id ='script.js'",
+            "<script srcs ='script.js'"
         ]
     },
     {
         numb: 4,
-        question:   "What is the collective noun for a group of pandas",
-        answer:     "Embarrassment",
+        question:   "The external JavaScript file must contain the <script> tag.",
+        answer:     "False",
         options: [
-            "clan",
-            "pride",
-            "sleuth"
+            "True",
+            "False",
+            "Maybe",
+            "Both True and False"
         ]
     },
     {
         numb: 5,
-        question:   "Who composed the music for Sonic the Hedgehog 3?",
-        answer:     "Michael Jackson",
+        question:   "How do you write 'Hello World' in an alert box?",
+        answer:     "alert('Hello World');",
         options: [
-            "Skrillex",
-            "Anderson Paak",
-            "Ryan Tedder"
+            "prompt('Hello World');",
+            "alert('Hello World');",
+            "confirm('Hello World');",
+            "console.log('Hello World');"
         ]
     },
     {
         numb: 6,
-        question:   "In Georgia State, what would be illegal to eat with a fork?",
-        answer:     "Fried Chicken",
+        question:   "How do you create a function in JavaScript?",
+        answer:     "function myFunction()",
         options: [
-            "Ribs",
-            "Fries",
-            "BBQ"
+            "function myFunction()",
+            "function = myFunction()",
+            "function() = myFunction",
+            "function:myFunction()"
         ]
     },
     {
         numb: 7,
-        question:   "Which sea creature have 3 hearts?",
-        answer:     "Octopus",
+        question:   "How to write an IF statement in JavaScript?",
+        answer:     "if (i==5)",
         options: [
-            "Whale",
-            "Lobster",
-            "Stingray"
+            "if (i==5)",
+            "if i=5",
+            "if i='5' then",
+            "if i==5 then"
         ]
     },
     {
         numb: 8,
-        question:   "First hockey pucks used in early outdoor hockey games were made of what?",
-        answer:     "frozen cowdung",
+        question:   "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        answer:     "if (i !=5)",
         options: [
-            "Rubberball",
-            "rocks",
-            "Wood"
+            "if (i !=5)",
+            "if i !=5 then",
+            "if i !=5",
+            "if {i !=5}"
         ]
     },
     {
         numb: 9,
-        question:   "The 'Tina' in Tina Fey is short for what name?",
-        answer:     "Stamatina",
+        question:   "How does a FOR loop start?",
+        answer:     "for (i = 0; i <= 5; i++)",
         options: [
-            "Christina",
-            "Albertina",
-            "Bettina"
+            "for (i <= 5; i++)",
+            "for i = 1 to 5 i++",
+            "for (i = 0; i <= 5; i++)",
+            "for (i <= 5; i++)"
         ]
     },
     {
         numb: 10,
-        question:   "Which European country technically shares a border with Brazil?",
-        answer:     "France because of French Guiana",
+        question:   "How do you declare a JavaScript variable?",
+        answer:     "var carName;",
         options: [
-            "Germany because of Pomerode",
-            "Portugal because of",
-            "None of the choices are correct, Brazil isn't close to Europe"
+            "v carName;",
+            "variable carName;",
+            "var carName;",
+            "var = carName;"
         ]
     },
 ];
@@ -141,17 +147,17 @@ let questions = [
 
 
 
-
+// Question is presented in dynamic html 
 function displayQuestion() {
-    showQuestionEl.innerHTML = `        
-    <h1 id="question-placeholder">${questions[index].question}</h1>
-    <div>
+    showQuestionEl.innerHTML =
+    `<h1 id="question-placeholder">${questions[index].question}</h1>
+     <div>
         <p><button class="choices-placeholder">1. ${questions[index].options[0]}</button></p>
         <p><button class="choices-placeholder">2. ${questions[index].options[1]}</button></p>
         <p><button class="choices-placeholder">3. ${questions[index].options[2]}</button></p>
         <p><button class="choices-placeholder">4. ${questions[index].options[3]}</button></p>
-    </div>
-`
+     </div>`
+
 var buttonChoices = document.querySelectorAll(".choices-placeholder");
 for (let i = 0; i < buttonChoices.length; i++) {
     buttonChoices[i].addEventListener("click", function(){
@@ -170,10 +176,6 @@ for (let i = 0; i < buttonChoices.length; i++) {
 }; 
 
 
-
-
-
-
 // Start Quiz
 var startQuiz = function() {
 console.log("works?");
@@ -182,7 +184,7 @@ infoBoxEl.style.display = 'none';
 timerId = setInterval(updateCountdown, 1000);
 };
 
-
+// eventlistener, quiz starts when start button is pressed
 buttonEl.addEventListener("click", startQuiz);
 submitButtonEl.addEventListener("click", function(){
     scores.push({
@@ -190,7 +192,7 @@ submitButtonEl.addEventListener("click", function(){
         score: time
     })
     localStorage.setItem("score", JSON.stringify(scores));
-})
+});
 
 
 
